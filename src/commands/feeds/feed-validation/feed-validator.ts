4621da9a077b55e52a8128b52e1e2d4f79be2575
Henrique Barbosa <mudgenone@gmail.com>
2022-08-27 00:08:36 -0300

    feat: Add URL validation

--- src/commands/feeds/feed-validation/feed-validator.ts
+++ src/commands/feeds/feed-validation/feed-validator.ts
@@ -5,7 +5,9 @@ class FeedValidator {
   readonly W3C_VALIDATOR_URL: string = 'https://validator.w3.org/feed/check.cgi';
   readonly OUTPUT: string = 'soap12';
 
-  async checkUrl(url: string) {
+  async checkUrl(url: string | null) {
+    this.validateUrl(url);
+
     const response = await getXmlFromUrl(`${this.W3C_VALIDATOR_URL}?output=${this.OUTPUT}&url=${url}`);
 
     const validity = this.extractValidity(response);
@@ -16,6 +18,22 @@ class FeedValidator {
   }
 
   private extractValidity = R.path(['env:Envelope', 'env:Body', 0, 'm:feedvalidationresponse', 0, 'm:validity', 0]);
+
+  private validateUrl(link: string | null) {
+    if (!link) {
+      throw new Error('No link informed!');
+    }
+
+    try {
+      const url = new URL(link);
+
+      if (url.protocol !== 'http:' && url.protocol !== 'https:') {
+        throw new Error('Malformed link!');
+      }
+    } catch (e) {
+      throw new Error('Link is not a URL!');
+    }
+  }
 }
 
 export default new FeedValidator();
